c2bc676e4691cb12d9347fe34a19ef51ac8beca7
Nicolas <nicolas.pp333@gmail.com>
2024-02-12 16:49:03 -0300

    Seccion 11 - Seguridad

--- TareasMVC/ApplicationDbContext.cs
+++ TareasMVC/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata.Ecma335;
 using TareasMVC.Entidades;
 
 namespace TareasMVC
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
---
+++ TareasMVC/Migrations/20240212175424_AdminRol.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"if not exists(select id from AspNetRoles where Id = 'd655635d-d812-495a-909a-547e23331c85')
+                begin
+	                insert AspNetRoles (Id, [NormalizedName], [Name])
+	                values ('d655635d-d812-495a-909a-547e23331c85', 'ADMIN', 'admin')
+                end");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete AspNetRoles where Id = 'd655635d-d812-495a-909a-547e23331c85'");
+        }
+    }
+}
