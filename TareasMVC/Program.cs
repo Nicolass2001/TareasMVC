35161bce88f4ec6c085bad430936fc181838a0e9
Nicolas <nicolas.pp333@gmail.com>
2024-02-18 16:06:16 -0300

    Class 272 Error

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -50,6 +50,7 @@ builder.Services.AddLocalization(options =>
 
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
 
 var app = builder.Build();
 
---
+++ TareasMVC/wwwroot/js/archivos-adjuntos.js
@@ -0,0 +1,31 @@
+﻿let inputArchivoTarea = document.getElementById('archivoTarea');
+
+function manejarClickAgregarArchivoAdjuto() {
+    inputArchivoTarea.click();
+}
+
+async function manejarSeleccionArchivoTarea(event) {
+    const archivos = event.target.files;
+    const archivosArreglo = Array.from(archivos);
+
+    const idTarea = tareaEditarVM.id;
+    const formData = new FormData();
+    for (var i = 0; i < archivosArreglo.length; i++) {
+        formData.append("archivos", archivosArreglo[i]);
+    }
+
+    const respuesta = await fetch(`${urlArchivos}/${idTarea}`, {
+        body: formData,
+        method: 'POST'
+    });
+
+    if (!respuesta.ok) {
+        manejarErrorApi(respuesta);
+        return;
+    }
+
+    const json = await respuesta.json();
+    console.log(json);
+
+    inputArchivoTarea.value = null;
+}
